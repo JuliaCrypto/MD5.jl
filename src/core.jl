c310e5bd6b3c379c4546fd72a028e08e16a1f3fa
Jan Weidner <jw3126@gmail.com>
2018-01-11 16:51:56 +0100

    cleanup

--- src/core.jl
+++ src/core.jl
@@ -1,65 +1,12 @@
-# Nonlinear functions, in order to encourage inlining, these sadly are not an array of lambdas
-f_round0(b,c,d) = SHA.Round0(b,c,d) #UInt32((b & c) | (~b & d))
-f_round1(b,c,d) = UInt32((b & d) | (c & ~d))
-f_round2(b,c,d) = SHA.Round1And3(b,c,d) # return UInt32(b ⊻ c ⊻ d) #xors
-f_round3(b,c,d) = UInt32(c ⊻ (b | ~d))
-
-g_round0(i) = i
-g_round1(i) = (5i+1) % 16
-g_round2(i) = (3i + 5) % 16
-g_round3(i) = 7i % 16
-
-
-function conclude_round(a,b,c,d,f,g,pbuf,i)
-    @inbounds s = ss[i+1]
-    @inbounds k = kk[i+1]
-    @inbounds m = unsafe_load(pbuf, g+1)
-    f = f + a + k + m
-    a = d
-    d = c
-    c = b
-    b = b + lrot(s,f, 32)
-    a,b,c,d
-end
-
-transform!(ctx::MD5_CTX) = transform_unrolled!(ctx)
-
-function transform_baseline!(context::MD5_CTX)
-   pbuf = buffer_pointer(context)
-   a,b,c,d = context.state
-     
-   for i in 0:15
-        f = f_round0(b,c,d)
-        g = g_round0(i)
-        a,b,c,d = conclude_round(a, b, c, d, f, g, pbuf, i)
-   end
-   for i in 16:31
-        f = f_round1(b,c,d)
-        g = g_round1(i)
-        a,b,c,d = conclude_round(a, b, c, d, f, g, pbuf, i)
-   end
-   for i in 32:47
-        f = f_round2(b,c,d)
-        g = g_round2(i)
-        a,b,c,d = conclude_round(a, b, c, d, f, g, pbuf, i)
-   end
-   for i in 48:63
-        f = f_round3(b,c,d)
-        g = g_round3(i)
-        a,b,c,d = conclude_round(a, b, c, d, f, g, pbuf, i)
-    end
-    @inbounds context.state .+= [a,b,c,d]
-end
-
-@generated function transform_unrolled!(context::MD5_CTX)
+@generated function transform!(context::MD5_CTX)
     ret = quote
         pbuf = buffer_pointer(context)
     end
     ex  = quote
-        A = context.state[1]
-        B = context.state[2]
-        C = context.state[3]
-        D = context.state[4]
+        @inbounds A = context.state[1]
+        @inbounds B = context.state[2]
+        @inbounds C = context.state[3]
+        @inbounds D = context.state[4]
     end
     push!(ret.args, ex)
     for i in 0:63
@@ -91,14 +38,14 @@ end
     end
 
     ex = quote
-        context.state[1] += A
-        context.state[2] += B
-        context.state[3] += C
-        context.state[4] += D
+        @inbounds context.state[1] += A
+        @inbounds context.state[2] += B
+        @inbounds context.state[3] += C
+        @inbounds context.state[4] += D
     end
     push!(ret.args, ex)
     quote
-        @inbounds $ret
+        $ret
     end
 end
 
