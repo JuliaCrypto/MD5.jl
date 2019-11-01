3db35f7faeadfdd2e00f21c1edb6ea27555e5cf2
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-11-01 12:30:10 +0000

    fix typo and whitespace

--- src/constants.jl
+++ src/constants.jl
@@ -1,6 +1,5 @@
-
 # Note: this code more or less comes directly from https://en.wikipedia.org/wiki/MD5
-# I believe this is fair use. and does not have lisence implications
+# I believe this is fair use. and does not have license implications
 
 const kk =  floor.(UInt32, Int64(2)^32 * abs.(sin.(1:64)))
 
@@ -11,8 +10,4 @@ const ss = UInt64[
 6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,
 ]
 
-
 const MD5_initial_hash_value = UInt32[0x67452301, 0xefcdab89, 0x98badcfe,  0x10325476] # A,B,C,D
-
-
-
